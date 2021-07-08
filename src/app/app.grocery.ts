d548117615cd8a44e8d8ab70cec764790a0bd634
Quynh <71189807+quynth@users.noreply.github.com>
2021-07-07 23:39:03 -0500

    Setup grocery component

---
+++ src/app/app.grocery.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-grocery',
+    templateUrl: './app.grocery.html',
+    styleUrls: [
+        '../assets/css/bootstrap.min.css'
+    ]
+})
+
+export class GroceryComponent {
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { GroceryComponent } from './app.grocery';
 
 @NgModule({
   declarations: [
-    AppComponent
+    GroceryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [GroceryComponent]
 })
 export class AppModule { }
